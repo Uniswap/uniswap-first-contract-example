9fdadc0bd04350761681febe8a711d7114872b0d
Alex Karys <akarys92@gmail.com>
2022-07-20 16:47:54 -0400

    Update SimpleSwap.sol

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -6,5 +6,6 @@ import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract SimpleSwap {
-   
+   constructor() {
+   }
 }
