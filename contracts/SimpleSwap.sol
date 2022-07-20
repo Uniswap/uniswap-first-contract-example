e6f0fc8082d476109bcb7b618eb2a54ba6e7b19d
Alex.Karys <alex.karys@UNISWAP-MAC-011.local>
2022-07-20 12:17:01 -0400

    Removing code for examples

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -6,35 +6,5 @@ import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract SimpleSwap {
-    ISwapRouter public immutable swapRouter;
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    uint24 public constant feeTier = 3000;
-    
-    constructor(ISwapRouter _swapRouter) {
-        swapRouter = _swapRouter;
-    }
-    
-    function swapWethForDAI(uint wEthIn) external returns (uint256 DAIOut) {
-
-        // Transfer the specified amount of WETH9 to this contract.
-        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), wEthIn);
-        // Approve the router to spend WETH9.
-        TransferHelper.safeApprove(WETH9, address(swapRouter), wEthIn);
-        // Create the params that will be used to execute the swap
-        ISwapRouter.ExactInputSingleParams memory params =
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: WETH9,
-                tokenOut: DAI,
-                fee: feeTier,
-                recipient: msg.sender,
-                deadline: block.timestamp,
-                amountIn: wEthIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
-        // The call to `exactInputSingle` executes the swap.
-        DAIOut = swapRouter.exactInputSingle(params);
-        return DAIOut; 
-    }
+   
 }
