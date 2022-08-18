db7ab6cacaff923306b71144a99cb345d954b28f
Alex Karys <akarys92@gmail.com>
2022-08-18 17:32:45 -0400

    Update SimpleSwap.test.js

--- test/SimpleSwap.test.js
+++ test/SimpleSwap.test.js
@@ -1,8 +1,9 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const DAI_DECIMALS = 18; 
 const SwapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; 
 
 const ercAbi = [
