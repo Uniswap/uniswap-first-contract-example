6db31bfa01c84b986cd9b6df6b31b64a9c4313cb
Alex.Karys <alex.karys@UNISWAP-MAC-011.local>
2022-08-18 17:29:00 -0400

    Making the test user written

--- test/SimpleSwap.test.js
+++ test/SimpleSwap.test.js
@@ -18,34 +18,25 @@ describe("SimpleSwap", function () {
   it("Should provide a caller with more DAI than they started with after a swap", async function () {
     
     /* Deploy the SimpleSwap contract */
-    const SimpleSwap = await ethers.getContractFactory("SimpleSwap");
-    const simpleSwap = await SimpleSwap.deploy(SwapRouterAddress);
-    await simpleSwap.deployed();
-    let signers = await hre.ethers.getSigners();
+
 
     /* Connect to weth9 and wrap some eth  */
-    const weth9 = new hre.ethers.Contract(wethAddress, ercAbi, signers[0]);
-    let transaction = await weth9.deposit({ value: hre.ethers.utils.parseEther("10") });
-    await transaction.wait();
+
     
     /* Check Initial DAI Balance */ 
-    const DAI = new hre.ethers.Contract(DAIAddress, ercAbi, signers[0]);
-    let daiInWalletStart = await DAI.balanceOf(signers[0].address);
-    daiInWalletStart = Number(hre.ethers.utils.formatEther(daiInWalletStart));
+
 
     /* Approve the swapper contract to spend weth9 for me */
-    let approval = await weth9.approve(simpleSwap.address, hre.ethers.utils.parseEther("1"));
+
     
     /* Execute the swap */
-    const amountIn = "0.1"; 
-    let txn = await simpleSwap.swapWethForDAI(hre.ethers.utils.parseEther(amountIn), { gasLimit: 300000 });
-    txn.wait(); 
+ 
     
     /* Check DAI end balance */
-    let daiInWalletEnd = await DAI.balanceOf(signers[0].address);
-    daiInWalletEnd = Number(hre.ethers.utils.formatEther(daiInWalletEnd));
+
     
-    expect( daiInWalletEnd )
-      .is.greaterThan(daiInWalletStart); 
+    /* Test that we now have more DAI than when we started */
+
+
   });
 });
