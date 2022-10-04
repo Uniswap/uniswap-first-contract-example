032dd35c4601dd000fe7457614c8c1a59b36a572
arche <arche@namu.finance>
2022-10-04 16:44:19 +0900

    feat: Implement single swap with uniswapV3

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,17 @@ import "@primitivefi/hardhat-dodoc";
 import "hardhat-contract-sizer";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+import dotenv from "dotenv";
+import { string } from "hardhat/internal/core/params/argumentTypes";
+dotenv.config();
+
+const {
+  ALCHEMY_GOERLI_URL,
+  ALCHEMY_MAIN_URL,
+  MY_META_MASK_ACCOUNT_PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+} = process.env;
 // require('hardhat-ethernal');
 
 // You need to export an object to set up your config
@@ -12,39 +23,50 @@ import "@nomicfoundation/hardhat-toolbox";
  */
 
 const config: HardhatUserConfig = {
-    solidity: {
+  solidity: {
+    compilers: [
+      {
         version: "0.8.11",
         settings: {
-            // Disable the optimizer when debugging
-            // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
+          // Disable the optimizer when debugging
+          // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
         },
+      },
+      {
+        version: "0.7.6",
+      },
+    ],
+  },
+
+  networks: {
+    hardhat: {
+      accounts: {
+        accountsBalance: "1000000000000000000000000",
+        count: 10,
+      },
+      forking: {
+        url: `${ALCHEMY_MAIN_URL}`,
+      },
     },
 
-    networks: {
-        hardhat: {
-            accounts: {
-                accountsBalance: "1000000000000000000000000",
-                count: 10,
-            },
-        },
-        localhost: {
-            url: "http://127.0.0.1:8545/",
-        },
-
-        coverage: {
-            url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
-        },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
     },
 
-    dodoc: {
-        runOnCompile: true,
-        debugMode: true,
-        outputDir: "./docs",
+    coverage: {
+      url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
     },
+  },
+
+  dodoc: {
+    runOnCompile: true,
+    debugMode: true,
+    outputDir: "./docs",
+  },
 };
 
 export default config;
